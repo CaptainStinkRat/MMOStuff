76fa6893b54d94800404c59317c6cd1e109d5c8b
CaptainStinkRat <116904070+CaptainStinkRat@users.noreply.github.com>
2023-09-13 14:52:14 -0500

    Update notifcation.py

--- notifcation.py
+++ notifcation.py
@@ -3,16 +3,20 @@ from tkinter import ttk
 import tkinter as tk
 import time
 import os
-win = Tk()
-win.geometry('800x480')
-Grid.rowconfigure(win,0,weight=1)
-Grid.columnconfigure(win,0,weight=1)
-Grid.rowconfigure(win,1,weight=1)
-Grid.columnconfigure(win,1,weight=1)
+# win = Tk()
+# win.geometry('800x480')
+# win.attributes('-fullscreen',True)
+# win.configure(background='black')
+# Grid.rowconfigure(win,0,weight=1)
+# Grid.columnconfigure(win,0,weight=1)
+# Grid.rowconfigure(win,1,weight=1)
+# Grid.columnconfigure(win,1,weight=1)
 
 def eatingGif():
     win.destroy()
     root = Tk()
+    root.configure(background='black')
+    root.attributes('-fullscreen',True)
     framecnt= 6
     frames = [PhotoImage(file='./Eating.gif',format='gif -index %i' %(i)) for i in range(framecnt)]
 
@@ -24,12 +28,16 @@ def eatingGif():
             ind = 0
         label.configure(image=frame)
         win.after(1,update,ind)
-    label = Label(root,width=800,height=480)
+    label = Label(root)
     label.pack()
+    title = Label(root,text='On lunch',font=('Arial',25),fg='#FFFFFF',bg='#000000')
+    title.pack()
     root.after(0,update,0)
 def walkGif():
     win.destroy()
     root = Tk()
+    root.configure(bg='black')
+    root.attributes('-fullscreen',True)
     root.title("On a walk")
     framecnt= 20
     frames = [PhotoImage(file='./walking.gif',format='gif -index %i' %(i)) for i in range(framecnt)]
@@ -42,12 +50,16 @@ def walkGif():
             ind = 0
         label.configure(image=frame)
         win.after(1,update,ind)
-    label = Label(root,width=800,height=480)
+    label = Label(root)
     label.pack()
+    title = Label(root,text='On a walk',font=('Arial',25),fg='#FFFFFF',bg='#000000')
+    title.pack()
     root.after(0,update,0)
 def meetingGif():
     win.destroy()
     root = Tk()
+    root.configure(bg='black')
+    root.attributes('-fullscreen',True)
     root.title("In a meeting")
     framecnt= 10
     frames = [PhotoImage(file='./meeting.gif',format='gif -index %i' %(i)) for i in range(framecnt)]
@@ -60,12 +72,16 @@ def meetingGif():
             ind = 0
         label.configure(image=frame)
         win.after(1,update,ind)
-    label = Label(root,width=800,height=480)
+    label = Label(root)
     label.pack()
+    title = Label(root,text='In a meeting',font=('Arial',25),fg='#FFFFFF',bg='#000000')
+    title.pack()
     root.after(0,update,0)
 def busyGif():
     win.destroy()
     root = Tk()
+    root.configure(bg='black')
+    root.attributes('-fullscreen',True)
     root.geometry('800x480')
     root.title("Busy")
     framecnt= 10
@@ -79,11 +95,26 @@ def busyGif():
             ind = 0
         label.configure(image=frame)
         win.after(1,update,ind)
-    label = Label(root,width=800,height=480)
+    label = Label(root,text='Busy working')
     label.pack()
+    title = Label(root,text='Busy',font=('Arial',25),fg='#FFFFFF',bg='#000000')
+    title.pack()
     root.after(0,update,0)
+    
+    
+    
 # def gifPlay():
 #     win.destroy()
+
+
+win = Tk()
+win.geometry('800x480')
+win.attributes('-fullscreen',True)  
+win.configure(background='black')
+Grid.rowconfigure(win,0,weight=1)
+Grid.columnconfigure(win,0,weight=1)
+Grid.rowconfigure(win,1,weight=1)
+Grid.columnconfigure(win,1,weight=1)
 lunchButton=ttk.Button(win,text = 'At lunch',command=eatingGif)
 lunchButton.grid(column=0,row=0,sticky="NSEW")
 walkButton = ttk.Button(win,text = 'On a walk',command=walkGif)
@@ -92,5 +123,6 @@ meetingButton=ttk.Button(win,text = 'In a meeting',command=meetingGif)
 meetingButton.grid(column=1,row=0,sticky='NSEW')
 busyButton=ttk.Button(win,text = 'Busy',command=busyGif)
 busyButton.grid(column=1,row=1,sticky='NSEW')
-# Button(win,text='At lunch',command=gifPlay).pack()
+    # Button(win,text='At lunch',command=gifPlay).pack()
 win.mainloop()
+
