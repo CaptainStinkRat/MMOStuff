9e37c38026aa979bc6328ea51c7bb10109fc6aef
CaptainStinkRat <116904070+CaptainStinkRat@users.noreply.github.com>
2023-10-06 16:42:23 -0500

    Update osrsTracker.py

--- osrsTracker.py
+++ osrsTracker.py
@@ -1,166 +1,67 @@
-import tkinter as tk
 from OSRSBytes import Hiscores
 from OSRSBytes import Items
-import json
-import tkinter.font as font
-import time
-from datetime import datetime
-from tkinter import *
+import argparse
 import os
+from datetime import datetime
+import json
 
-class Menu(tk.Tk):
-    def __init__(self):
-        super().__init__()
-        self.geometry('800x480')
-        self.attributes('-fullscreen',True)
-        self.configure(background='black')
-        self.myFont = font.Font(family='Helvetica' ,size=35,weight='bold')
-        # self.timeLabel = tk.Label(self,text='0.00')
-        # self.timeLabel.pack()
-        # self.timeLabel['font'] = self.myFont
-        self.isRunning = False
-        self.startTime = time.time()
-        self.bossUser = Hiscores('dad_pvmer')
-        self.createWidgets()
-    def createWidgets(self):    
-        # self.startButton = Button(self,text='Start timer',command=self.start)
-        # self.startButton['font'] =self.myFont
-        # self.startButton.pack()
-        self.wintertodtButton = Button(self,text='Wintertodt',command=self.wintertodtStart)
-        self.wintertodtButton['font'] = self.myFont
-        self.wintertodtButton.grid(column=0,row=0)
-        self.kbdButton = Button(self,text='King Black Dragon',command=self.kbdStart)
-        self.kbdButton['font']=self.myFont
-        self.kbdButton.grid(column=1,row=0)
-        self.giantMoleButton = Button(self,text='Giant Mole',command=self.giantMoleStart)
-        self.giantMoleButton['font']=self.myFont
-        self.giantMoleButton.grid(column=0,row=1)
-        self.kalphiteQueenButton = Button(self,text='Kalphite Queen',command=self.kqStart)
-        self.kalphiteQueenButton['font']=self.myFont
-        self.kalphiteQueenButton.grid(column=1,row=1)
-        self.cerberusButton = Button(self,text='Cerberus',command=self.cerberusStart)
-        self.cerberusButton['font']=self.myFont
-        self.cerberusButton.grid(column=2,row=1)
-    def updateTime(self):
-        if self.isRunning:
-            self.elapsedTime=time.time() - self.startTime
-            self.timeLabel.config(text="{:.2f}".format(self.elapsedTime))
-            self.timeLabel.after(50,self.updateTime)
-    def wintertodtStart(self):
-        self.boss = 'wintertodt'
-        self.bossStartNumber = self.bossUser.boss(f'{self.boss}','score')
-        self.wintertodtButton.destroy()
-        self.kbdButton.destroy()
-        self.giantMoleButton.destroy()
-        self.kalphiteQueenButton.destroy()
-        self.cerberusButton.destroy()
-        self.startButton = Button(self,text='Start timer',command=self.start)
-        self.startButton['font'] =self.myFont
-        self.startButton.grid(column=2,row=1)
-
-    def kbdStart(self):
-        self.boss = 'KingBlackDragon'
-        self.bossStartNumber = self.bossUser.boss(f'{self.boss}','score')
-        self.wintertodtButton.destroy()
-        self.kbdButton.destroy()
-        self.giantMoleButton.destroy()
-        self.kalphiteQueenButton.destroy()
-        self.cerberusButton.destroy()
-        self.startButton = Button(self,text='Start timer',command=self.start)
-        self.startButton['font'] =self.myFont
-        self.startButton.grid(column=2,row=1)
-
-    def giantMoleStart(self):
-        self.boss = 'GiantMole'
-        self.bossStartNumber = self.bossUser.boss(f'{self.boss}','score')
-        self.wintertodtButton.destroy()
-        self.kbdButton.destroy()
-        self.giantMoleButton.destroy()
-        self.kalphiteQueenButton.destroy()
-        self.cerberusButton.destroy()
-        self.startButton = Button(self,text='Start timer',command=self.start)
-        self.startButton['font'] =self.myFont
-        self.startButton.grid(column=2,row=1)
-    
-    def startButtonAdd(self):
-        self.wintertodtButton.destroy()
-        self.kbdButton.destroy()
-        self.giantMoleButton.destroy()
-        self.kalphiteQueenButton.destroy()
-        self.cerberusButton.destroy()
-        self.startButton = Button(self,text='Start timer',command=self.start)
-        self.startButton['font'] =self.myFont
-        self.startButton.grid(column=2,row=1)
-
-    def kqStart(self):
-        self.boss = 'KalphiteQueen'
-        self.bossStartNumber = self.bossUser.boss(f'{self.boss}','score')
-        self.wintertodtButton.destroy()
-        self.kbdButton.destroy()
-        self.giantMoleButton.destroy()
-        self.kalphiteQueenButton.destroy()
-        self.cerberusButton.destroy()
-        self.startButton = Button(self,text='Start timer',command=self.start)
-        self.startButton['font'] =self.myFont
-        self.startButton.grid(column=2,row=1)
-
-    def cerberusStart(self):
-        self.boss = 'cerberus'
-        self.bossStartNumber = self.bossUser.boss(f'{self.boss}','score')
-        self.wintertodtButton.destroy()
-        self.kbdButton.destroy()
-        self.giantMoleButton.destroy()
-        self.kalphiteQueenButton.destroy()
-        self.cerberusButton.destroy()
-        self.startButton = Button(self,text='Start timer',command=self.start)
-        self.startButton['font'] =self.myFont
-        self.startButton.grid(column=2,row=1)
-    def start(self):
-        self.startButton.destroy()
-        self.stopButton = Button(self,text='Stop timer',command=self.stop)
-        self.stopButton['font'] =self.myFont
-        self.stopButton.grid(column=2,row=1)
-        self.timeLabel = tk.Label(self,text='0.00')
-        self.timeLabel.grid(column=2,row=3)
-        self.timeLabel['font'] = self.myFont
-        sTime = datetime.now()
-        startTime = sTime.strftime("%H:%M:%S")
+def bossStart(args):
+    lookupUser = args.user
+    bossingUser = Hiscores(lookupUser)
+    bossStartNumber = bossingUser.boss(f'{str(args.boss)}','score')
+    print("Starting boss number is: "+str(bossStartNumber))
+    sTime = datetime.now()
+    startTime = sTime.strftime("%H:%M:%S")
+    log ={
+        "Boss":f"{str(args.boss)}",
+        "User":f"{lookupUser}",
+        "bossTime":f"{startTime}",
+        "bossStartNumber":f"{bossStartNumber}"
+    }
+    jsonObject = json.dumps(log,indent=4)
+    with open("bossLog.json",'w') as outfile:
+        outfile.write(jsonObject)
+def bossFinish(args):
+    lookupUser = args.user
+    dataLog = open('bossLog.json')
+    data = json.load(dataLog)
+    os.remove("bossLog.json")
+    if data['User'] == f'{str(lookupUser)}':
+        bossingUserStats = data['User']
+        bossUser = Hiscores(bossingUserStats)
+        bossingUserStart=data['bossStartNumber']
+        boss = data['Boss']
+        bossFinish = bossUser.boss(f'{boss}')
+        bossTotal = int(bossingUserStart)-int(bossFinish)
+        fTime = datetime.now()
+        finishTime = fTime.strftime("%H:%M:%S")
+        #finishTimeStamp = int(finishTime)
+        dateStr = data['bossTime']
+        #startTimeStamp = int(dateStr)
+        sT = datetime.strptime(dateStr,"%H:%M:%S")
+        fT = datetime.strptime(finishTime,"%H:%M:%S")
+        totalTime = fT - sT
+        print("Time lapsed: "+str(totalTime)+" and "+str(bossTotal)+" killed in that time.")
         log ={
-            "User":"dad_pvmer",
-            "bossTime":f"{startTime}",
-            "bossStartNumber":f"{self.bossStartNumber}"
+            "Boss":f"{str(args.boss)}",
+            "User":f"{bossingUserStats}",
+            "timeLapsed":f"{totalTime}",
+            "totalKilled":f"{bossTotal}"
         }
         jsonObject = json.dumps(log,indent=4)
-        with open("bossLog.json",'w') as outfile:
+        with open("bossEndLog.json",'w') as outfile:
             outfile.write(jsonObject)
-        if not self.isRunning:
-            self.isRunning = True
-            self.startTime = time.time()
-            self.updateTime()
-    def stop(self):
-        self.isRunning = False
-        dataLog = open('bossLog.json')
-        data = json.load(dataLog)
-        endingBossCount = self.bossUser.boss(f'{self.boss}','score')
-        self.bossEndNumber = self.bossStartNumber - endingBossCount
-        if data['User'] == 'dad_pvmer':
-            fTime = datetime.now()
-            finishTime = fTime.strftime("%H:%M:%S")
-        #finishTimeStamp = int(finishTime)
-            dateStr = data['bossTime']
-        #startTimeStamp = int(dateStr)
-            sT = datetime.strptime(dateStr,"%H:%M:%S")
-            fT = datetime.strptime(finishTime,"%H:%M:%S")
-            totalTime = fT - sT
-            finishedLog = {
-                "User":"dad_pvmer",
-                "bossTotalTime":f'{totalTime}',
-                'numberOfBossesKilled':f'{self.bossEndNumber}'
-            }
-            jsonObject = json.dumps(finishedLog,indent=4)
-            with open("finishedBossLog.json",'w') as outfile:
-                outfile.write(jsonObject)
-        self.destroy()
-if __name__=='__main__':
-    app=Menu()
-    app.mainloop()
+def main():
+    parser = argparse.ArgumentParser(description='Boss Tracker')
+    parser.add_argument("-s","--start",default = None,action="store_true",help='Start of the bossing log')
+    parser.add_argument("-f","--finish",default=None,action='store_true',help='Finish of bossing time')
+    parser.add_argument('user',help='User to keep track of boss kills for')
+    parser.add_argument('boss',help='Boss to keep track of')
+    args= parser.parse_args()
+    if args.start != None:
+        bossStart(args)
+    if args.finish != None:
+        bossFinish(args)
+
+if __name__ == '__main__':
+    main()
